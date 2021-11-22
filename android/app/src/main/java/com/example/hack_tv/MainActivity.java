301692e271e6fc248aed7a075297cd45194ad764
icamano <camanoi@uninorte.edu.co>
2021-11-22 18:19:33 -0500

    Initial commit

---
+++ android/app/src/main/java/com/example/hack_tv/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.hack_tv;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+void main(){
+  runApp(Center(
+    child: Text(
+      'Hack TV',
+      textDirection: TextDirection.ltr,
+    ),
+  )
+  );
+}
